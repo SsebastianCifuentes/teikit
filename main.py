9bf97a9322ecf6937e9f0c3035796a3b393c1e79
JonathanVegafz <jonathan.vega.f@mail.pucv.cl>
2024-02-11 14:13:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import socketio
 import flet as ft
 import asyncio
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
 
 sio = socketio.AsyncClient()
 
 # Configuración de la biblioteca RPi.GPIO
-GPIO.setmode(GPIO.BOARD)
-pines_relays = [16, 11, 12, 7, 10, 5, 8, 3, 26, 21, 24, 19, 22, 15, 18, 13]
-for pin in pines_relays:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.HIGH)
+# GPIO.setmode(GPIO.BOARD)
+# pines_relays = [16, 11, 12, 7, 10, 5, 8, 3, 26, 21, 24, 19, 22, 15, 18, 13]
+# for pin in pines_relays:
+#     GPIO.setup(pin, GPIO.OUT)
+#     GPIO.output(pin, GPIO.HIGH)
 
 async def main(page: ft.Page):
     await sio.connect('http://192.168.0.17:3030')
@@ -28,18 +28,18 @@ async def main(page: ft.Page):
                 En el casillero {data['id_casillero']}
             """
         # Abre el casillero
-            try:
-                seleccion = int(data['id_casillero'])
-                if 1 <= seleccion <= 16:
-                    GPIO.output(pines_relays[seleccion - 1], GPIO.LOW)
-                    print(f"Casillero {seleccion} activado.")
-                    await asyncio.sleep(8)  # Espera un momento
-                    GPIO.output(pines_relays[seleccion - 1], GPIO.HIGH)
-                    print(f"Casillero {seleccion} desactivado.")
-                else:
-                    print("ID de casillero no válido. Seleccione un número entre 1 y 16.")
-            except ValueError:
-                print("ID de casillero no válido. Debe ser un número entre 1 y 16.")
+            # try:
+            #     seleccion = int(data['id_casillero'])
+            #     if 1 <= seleccion <= 16:
+            #         GPIO.output(pines_relays[seleccion - 1], GPIO.LOW)
+            #         print(f"Casillero {seleccion} activado.")
+            #         await asyncio.sleep(8)  # Espera un momento
+            #         GPIO.output(pines_relays[seleccion - 1], GPIO.HIGH)
+            #         print(f"Casillero {seleccion} desactivado.")
+            #     else:
+            #         print("ID de casillero no válido. Seleccione un número entre 1 y 16.")
+            # except ValueError:
+            #     print("ID de casillero no válido. Debe ser un número entre 1 y 16.")
 
         await page.add_async(ft.Text(mensaje))
 
