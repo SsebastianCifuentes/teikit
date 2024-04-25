d8abd98e738c04c55f72f64f18afc4e3c8162c29
JonathanVegafz <jonathan.vega.f@mail.pucv.cl>
2024-04-25 12:59:43 -0400

    Update test.py

--- test.py
+++ test.py
@@ -30,7 +30,7 @@ async def main(page: ft.Page):
         await page.add_async(ft.Text("Abriendo casillero..."))
         
 
-        seleccion = 1
+        seleccion = int(data)
         print("la seleccion es: ", seleccion)
         GPIO.output(pines_relays[seleccion - 1], GPIO.LOW)
         await asyncio.sleep(8)
