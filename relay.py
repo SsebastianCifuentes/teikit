ab9181e4bf01c345c745e3fdb1c8f0e3abfc33e9
SsebastianCifuentes <ssebastian.cifuentes@gmail.com>
2024-12-18 08:40:05 -0300

    prueba

--- relay.py
+++ relay.py
@@ -11,7 +11,7 @@ try:
     # Configuración de los pines GPIO como salidas
     for pin in pines_relays:
         GPIO.setup(pin, GPIO.OUT)
-        GPIO.output(pin, GPIO.HIGH)
+        GPIO.output(pin, GPIO.LOW)
 
     while True:
         print("Seleccione un relé (1-16) o presione 'q' para salir:")
@@ -24,14 +24,14 @@ try:
             seleccion = int(seleccion)
             if 1 <= seleccion <= 16:
                 # Activar el relé seleccionado
-                GPIO.output(pines_relays[seleccion - 1], GPIO.LOW)
+                GPIO.output(pines_relays[seleccion - 1], GPIO.HIGH)
                 print(f"Relé {seleccion} activado.")
                 
                 # Esperar un momento (puedes ajustar según sea necesario)
                 time.sleep(3)
                 
                 # Desactivar el relé
-                GPIO.output(pines_relays[seleccion - 1], GPIO.HIGH)
+                GPIO.output(pines_relays[seleccion - 1], GPIO.LOW)
                 print(f"Relé {seleccion} desactivado.")
             else:
                 print("Seleccione un número entre 1 y 16.")
