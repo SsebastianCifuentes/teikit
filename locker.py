da53ccdf0bce7bebf8eb9be4c50f2a44894df7ff
SsebastianCifuentes <ssebastian.cifuentes@gmail.com>
2025-01-13 18:32:27 -0300

    Creacion de locker

--- locker.py
+++ locker.py
@@ -6,7 +6,7 @@ import os
 from flask import Flask, request, jsonify, abort
 from dotenv import load_dotenv
 from signal import signal, SIGINT
-from threading import Thread
+from threading import Thread, Lock
 
 # Cargar configuración del token desde variables de entorno
 load_dotenv()
@@ -22,9 +22,6 @@ TOTAL_LOCKERS = 16  # Número total de casilleros
 relay_pins = {1: 7, 2: 12, 3: 15, 4: 16, 5: 18, 6: 22, 7: 24, 8: 26,
               9: 31, 10: 32, 11: 33, 12: 35, 13: 36, 14: 37, 15: 38, 16: 40}
 
-# Crear la aplicación Flask
-app = Flask(__name__)
-
 # Configuración de los pines GPIO
 GPIO.setmode(GPIO.BOARD)
 def configure_gpio(pins):
@@ -34,12 +31,27 @@ def configure_gpio(pins):
 
 configure_gpio(relay_pins.values())
 
-# Abrir un casillero por 3 segundos
-def open_locker_gpio(locker_number):
-    pin = relay_pins[locker_number]
-    GPIO.output(pin, GPIO.HIGH)
-    time.sleep(3)
-    GPIO.output(pin, GPIO.LOW)
+# Bloqueo para acceso seguro a GPIO
+gpio_lock = Lock()
+
+# Función compartida para manejar GPIO
+def control_gpio(locker_number=None, all_lockers=False):
+    with gpio_lock:  # Asegurar acceso exclusivo
+        if all_lockers:
+            print("Abriendo todos los casilleros")
+            for pin in relay_pins.values():
+                GPIO.output(pin, GPIO.HIGH)
+            time.sleep(3)
+            for pin in relay_pins.values():
+                GPIO.output(pin, GPIO.LOW)
+            print("Todos los casilleros cerrados")
+        elif locker_number:
+            pin = relay_pins[locker_number]
+            print(f"Abriendo casillero {locker_number} en pin {pin}")
+            GPIO.output(pin, GPIO.HIGH)
+            time.sleep(3)
+            GPIO.output(pin, GPIO.LOW)
+            print(f"Casillero {locker_number} cerrado")
 
 # Notificar a la API externa
 def notify_external_api(locker_number):
@@ -62,7 +74,7 @@ def notify_external_api(locker_number):
 # --------------------------------------
 def start_ui():
     def open_locker_ui(locker_number):
-        open_locker_gpio(locker_number)
+        control_gpio(locker_number=locker_number)
         notify_external_api(locker_number)
 
     def on_closing():
@@ -80,23 +92,23 @@ def start_ui():
     root.geometry(f"{screen_width}x{screen_height}+0+0")
 
     # Configurar la ventana para que sea borderless
-    root.overrideredirect(True)  # Elimina el borde de la ventana
+    root.overrideredirect(True)
 
     root.protocol("WM_DELETE_WINDOW", on_closing)
 
-    # Crear un botón de "Cerrar" que estará en la parte superior de la ventana
+    # Crear un botón de "Cerrar"
     close_button = tk.Button(
         root,
         text="Cerrar",
         font=("Helvetica", 20),
         command=on_closing
     )
-    close_button.grid(row=0, column=0, padx=10, pady=10, sticky="nw")  # Se coloca en la esquina superior izquierda
+    close_button.grid(row=0, column=0, padx=10, pady=10, sticky="nw")
 
     # Configurar la cuadrícula para los botones de los casilleros
-    for i in range(TOTAL_LOCKERS // 4 + 1):  # Configurar filas
+    for i in range(TOTAL_LOCKERS // 4 + 1):
         root.grid_rowconfigure(i, weight=1)
-    for j in range(4):  # Máximo 4 columnas
+    for j in range(4):
         root.grid_columnconfigure(j, weight=1)
 
     # Crear botones para cada casillero
@@ -108,7 +120,7 @@ def start_ui():
             font=("Helvetica", 20),
             command=lambda ln=locker_number: open_locker_ui(ln)
         )
-        button.grid(row=row + 1, column=col, padx=10, pady=10, sticky="nsew")  # Desplazar las filas para dejar espacio al botón de cerrar
+        button.grid(row=row + 1, column=col, padx=10, pady=10, sticky="nsew")
 
     root.mainloop()
 
@@ -147,24 +159,17 @@ def open_locker():
         if not isinstance(locker, int) or locker < 1 or locker > TOTAL_LOCKERS:
             abort(400, description=f"Casillero inválido. Seleccione entre 1 y {TOTAL_LOCKERS}.")
 
-        pin = relay_pins[locker]
-        GPIO.output(pin, GPIO.HIGH)
-        time.sleep(3)
-        GPIO.output(pin, GPIO.LOW)
+        control_gpio(locker_number=locker)
 
         return jsonify({"status": f"Casillero {locker} abierto con éxito"}), 200
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 @app.route('/locker/opening/all', methods=['POST'])
 def open_all_lockers():
     try:
-        for pin in relay_pins.values():
-            GPIO.output(pin, GPIO.HIGH)
-        time.sleep(3)  # Mantén los pines activados durante 3 segundos
-        for pin in relay_pins.values():
-            GPIO.output(pin, GPIO.LOW)
-
+        control_gpio(all_lockers=True)
         return jsonify({"status": "Todos los casilleros han sido abiertos"}), 200
 
     except Exception as e:
@@ -181,5 +186,8 @@ signal(SIGINT, cleanup_gpio)
 # Iniciar Flask y Tkinter
 # --------------------------------------
 if __name__ == '__main__':
-    Thread(target=start_ui).start()
-    app.run(host='0.0.0.0', port=5000)
+    try:
+        Thread(target=start_ui, daemon=True).start()
+        app.run(host='0.0.0.0', port=5000)
+    finally:
+        GPIO.cleanup()
