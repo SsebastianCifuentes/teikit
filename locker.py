0a0ba79a6bbe98ec2f97894b3844ade907d9c61b
SsebastianCifuentes <ssebastian.cifuentes@gmail.com>
2025-01-13 18:45:22 -0300

    Creacion de locker

--- locker.py
+++ locker.py
@@ -165,5 +165,5 @@ signal(SIGINT, cleanup_gpio)
 # Iniciar Flask y Tkinter
 # --------------------------------------
 if __name__ == '__main__':
-    Thread(target=start_ui).start()
+    main()
     app.run(host='0.0.0.0', port=5000)
