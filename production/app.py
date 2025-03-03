a0bb6c94e92dc7ed6ef865fcdfafd914b01f699a
Teikit <teikit.app@gmail.com>
2025-03-03 20:11:56 -0300

    test

--- production/app.py
+++ production/app.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 from flask import Flask, request, jsonify, abort
 from signal import signal, SIGINT
 from ui import start_ui
+from gpio_controller import TOTAL_LOCKERS
 from config import API_TOKEN
 from threading import Thread
 import time
