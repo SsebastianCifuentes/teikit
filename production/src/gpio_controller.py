4e8ecc00f8fd577385ae3be6297ef805e415befd
Teikit <teikit.app@gmail.com>
2025-03-10 22:46:15 -0300

    cambio LOW to HIGH

--- production/src/gpio_controller.py
+++ production/src/gpio_controller.py
@@ -9,8 +9,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 relay_pins = {
-    1: 7, 2: 12, 3: 15, 4: 16, 5: 18, 6: 22, 7: 24, 8: 26,
-    9: 31, 10: 32, 11: 33, 12: 35, 13: 36, 14: 37, 15: 38, 16: 40
+    1: 26, 2: 24, 3: 22, 4: 18, 5: 16, 6: 15, 7: 12, 8: 7,
 }
 
 TOTAL_LOCKERS = len(relay_pins)
@@ -18,17 +17,17 @@ TOTAL_LOCKERS = len(relay_pins)
 # Inicializar pines solo una vez
 for pin in relay_pins.values():
     GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.LOW)
+    GPIO.output(pin, GPIO.HIGH)
 
 def turn_on_locker(locker_number):
     pin = relay_pins.get(locker_number)
     if pin:
-        GPIO.output(pin, GPIO.HIGH)
+        GPIO.output(pin, GPIO.LOW)
 
 def turn_off_locker(locker_number):
     pin = relay_pins.get(locker_number)
     if pin:
-        GPIO.output(pin, GPIO.LOW)
+        GPIO.output(pin, GPIO.HIGH)
 
 def open_all_lockers_api():
     def open_and_close_locker(locker_number, delay):
